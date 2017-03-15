cacbaaf9d0beeff400e37158b515ebc377d2be7a
Robert Schneiderman <robert.a.schneiderman@gmail.com>
2017-03-14 22:31:54 -0700

    album list fully structured

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,18 +1,49 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image, image} = album;
+    let { 
+        thumbnailStyle, 
+        headerContentStyle,
+        thumbnailContainerStyle
+    } = styles;
+
     return (
-        <View>
-            <Text>{props.album.title}</Text>
-        </View>
+        <Card>
+            <CardSection>
+                <View>
+                    <Image 
+                        style={styles.thumbnail_style}
+                        source={{ uri: thumbnail_image }} />
+                </View>
+                <View style={styles.headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+        </Card>
     );
 };
 
-const style = {
-    viewStyle: {
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
     },
     textStyle: {
+    },
+    thumbnail_style: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
     }
 };
 
