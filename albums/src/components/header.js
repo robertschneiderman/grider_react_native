917b0d1e1e592684fd4c26f592e13d7710fd26c6
Robert Schneiderman <robert.a.schneiderman@gmail.com>
2017-03-14 21:06:36 -0700

    styling of header component

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
-    return <Text>Albums!</Text>;
+    let { textStyle, viewStyle } = style;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums!</Text>
+        </View>
+    );
+};
+
+const style = {
+    viewStyle: {
+        backgroundColor: '#f8f8f8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20
+    }
 };
 
 export default Header;
